c940ac510b68dd52a018b63dcce6c5c601cb763f
ShafinRME <shafin.rmedu4@gmail.com>
2022-02-28 23:26:35 +0600

    search and giving all result

--- phone-hunter.js
+++ phone-hunter.js
@@ -5,6 +5,29 @@ const searchPhone = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data));
+        .then(data => displaySearchResult(data.data));
 
+}
+
+const displaySearchResult = data => {
+    const searchResult = document.getElementById('search-result');
+    data.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+            <img class="w-50 mx-auto" src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title text-center">Name: ${phone.phone_name}</h5>
+                <p class="card-text text-center">Brand: ${phone.brand}</p>
+                <div class="text-center">
+                <button type="button" class="btn btn-danger ">Details...</button>
+                </div>
+            </div>
+        </div>
+        
+        `;
+        searchResult.appendChild(div);
+    })
 }
