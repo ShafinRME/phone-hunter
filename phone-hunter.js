a78116875855ea5955cfd1847323e299e8db484d
ShafinRME <shafin.rmedu4@gmail.com>
2022-03-01 00:41:00 +0600

    Details is nearly finished.

--- phone-hunter.js
+++ phone-hunter.js
@@ -12,7 +12,6 @@ const searchPhone = () => {
 const displaySearchResult = data => {
     const searchResult = document.getElementById('search-result');
     data.forEach(phone => {
-        console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -22,7 +21,7 @@ const displaySearchResult = data => {
                 <h5 class="card-title text-center">Name: ${phone.phone_name}</h5>
                 <p class="card-text text-center">Brand: ${phone.brand}</p>
                 <div class="text-center">
-                <button type="button" class="btn btn-danger ">Details...</button>
+                <button onclick="loadMealDetail('${phone.slug}')" type="button" class="btn btn-danger ">Details...</button>
                 </div>
             </div>
         </div>
@@ -30,4 +29,34 @@ const displaySearchResult = data => {
         `;
         searchResult.appendChild(div);
     })
+}
+
+const loadMealDetail = phoneId => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => disPlayPhoneDetail(data.data));
+}
+
+const disPlayPhoneDetail = phone => {
+    console.log(phone);
+    const phoneDetails = document.getElementById('phone-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img class="w-50 mx-auto my-4" src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Brand: ${phone.brand}</h5>
+                <p class="card-text">Realease Date: ${phone.releaseDate}</p>
+                <h6 class="text-center">Features</h6>
+                <ul>
+                <li>Display Size:${phone.mainFeatures.displaySize}</li>
+                <li>Memory: ${phone.mainFeatures.memory}</li>
+                <li>Storage: ${phone.mainFeatures.storage}</li>
+                <li>Sensors: ${phone.mainFeatures.sensors}</li>
+                </ul>
+            </div>
+    
+    `;
+    phoneDetails.appendChild(div);
 }
