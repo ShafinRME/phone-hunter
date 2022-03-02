0cf37f3eb3eb09a60dc001eb4c085dd37ea3a067
ShafinRME <shafin.rmedu4@gmail.com>
2022-03-02 22:02:40 +0600

    Alhamdulillah.FInished

--- phone-hunter.js
+++ phone-hunter.js
@@ -15,14 +15,18 @@ const displaySearchResult = data => {
     if (data.length == 0) {
         const resultNot = document.getElementById('noResult');
         resultNot.style.display = 'block';
+        const moreResult = document.getElementById('moreResult');
+        moreResult.style.display = 'none';
     }
     else {
-        const resultNot = document.getElementById('noResult');
-        resultNot.style.display = 'none';
-        data.forEach(phone => {
-            const div = document.createElement('div');
-            div.classList.add('col');
-            div.innerHTML = `
+        if (data.length <= 20) {
+            const resultNot = document.getElementById('noResult');
+            resultNot.style.display = 'none';
+            moreResult.style.display = 'none';
+            data.forEach(phone => {
+                const div = document.createElement('div');
+                div.classList.add('col');
+                div.innerHTML = `
             <div class="card">
                 <img class="w-50 mx-auto" src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body">
@@ -35,11 +39,22 @@ const displaySearchResult = data => {
             </div>
             
             `;
-            searchResult.appendChild(div);
-        })
+                searchResult.appendChild(div);
+
+            })
+        }
+        else {
+            const moreResult = document.getElementById('moreResult');
+            moreResult.style.display = 'block';
+            const resultNot = document.getElementById('noResult');
+            resultNot.style.display = 'none';
+
+        }
+
     }
 
 }
+
 
 const loadPhoneDetail = phoneId => {
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
@@ -51,21 +66,37 @@ const loadPhoneDetail = phoneId => {
 const disPlayPhoneDetail = phone => {
     console.log(phone);
     const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.textContent = '';
     const div = document.createElement('div');
     div.classList.add('card');
-
     div.innerHTML = `
     <img class="w-50 mx-auto my-4" src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Brand: ${phone.brand}</h5>
-                <p class="card-text">Realease Date: ${phone.releaseDate}</p>
+                <p class="card-text">Realease Date: ${phone.releaseDate} </p>
                 <h6 class="text-center">Features</h6>
                 <ul>
                 <li>Display Size:${phone.mainFeatures.displaySize}</li>
                 <li>Memory: ${phone.mainFeatures.memory}</li>
                 <li>Storage: ${phone.mainFeatures.storage}</li>
-                <li>Sensors: ${phone.mainFeatures.sensors}</li>
+                <li>Sensors:
+                <ul>
+                <li>${phone.mainFeatures.sensors[0]}</li>
+                <li>${phone.mainFeatures.sensors[1]}</li>
+                <li>${phone.mainFeatures.sensors[2]}</li>
+                <li>${phone.mainFeatures.sensors[3]}</li>
                 </ul>
+                </ul>
+                <h6 class="text-center">Others</h6>
+                <ul>
+                <li>Bluetooth: ${phone.others.Bluetooth}</li>
+                <li>GPS: ${phone.others.GPS}</li>
+                <li>NFC: ${phone.others.NFC}</li>
+                <li>Radio: ${phone.others.Radio}</li>
+                <li>USB: ${phone.others.USB}</li>
+                <li>WLAN: ${phone.others.WLAN}</li>
+                </ul>
+
             </div>
     
     `;
